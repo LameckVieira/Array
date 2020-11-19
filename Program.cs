0fa604b7dd86a75689dd7b7527b1485b83ef42bd
LameckVieira <lameck.v.barbosa@gmail.com>
2020-11-19 11:12:41 -0300

    Exemplo de array

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Array");
+
+            string [] nomes =  new string[1];
+            int[] idade = new int[1];
+
+            for (var contador = 0; contador < 1; contador++)
+            {
+                Console.WriteLine($"Digite o {contador+1}º nome");
+                nomes[contador] = Console.ReadLine();
+                Console.WriteLine("Digite a idade");
+                idade[contador] = int.Parse(Console.ReadLine());
+            }
+                Console.WriteLine("Nomes Cadastrados:");
+            
+            for(var contador = 0; contador < 1; contador++){
+                Console.WriteLine("Nome: "+nomes[contador]);
+                Console.WriteLine("Idade: "+idade[contador]);
+
+            }
+                
+
+        }
+    }
+}
